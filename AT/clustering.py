c2ccdb7da3a851e4bf9e3b1572465f4b5e1452e6
Gleb <31935368+lobzison@users.noreply.github.com>
2017-12-28 16:00:43 +0100

    Update clustering.py

--- AT/clustering.py
+++ AT/clustering.py
@@ -60,8 +60,18 @@ def fast_closest_pair(cluster_list):
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] have minimum distance dist.       
     """
-    
-    return ()
+    num_clusters = len(cluster_list)
+    if num_clusters <= 3:
+        return slow_closest_pair(cluster_list)
+    middle = num_clusters // 2
+    left_part = [cluster for cluster in cluster_list[:middle]]
+    right_part = [cluster for cluster in cluster_list[middle:]]
+    result_left = fast_closest_pair(left_part)
+    result_right = fast_closest_pair(right_part)
+    result = min(result_left, (result_right[0], result_right[1] + middle, result_right[2] + middle))
+    mid = (cluster_list[middle - 1].horiz_center() + cluster_list[middle].horiz_center()) / 2
+    result = min(result, closest_pair_strip(cluster_list, mid, result[0]))
+    return result
 
 
 def closest_pair_strip(cluster_list, horiz_center, half_width):
@@ -76,11 +86,32 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
     Output: tuple of the form (dist, idx1, idx2) where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] lie in the strip and have minimum distance dist.       
     """
+    center_area_clusters = [cluster for cluster in cluster_list 
+                            if abs(cluster.horiz_center() - horiz_center) < half_width]
+    center_area_clusters.sort(key = lambda cluster: cluster.vert_center())
+    size = len(center_area_clusters)
+    result = (float("inf"), -1, -1)
+    for cluster_idx1 in range(size - 1):
+        for cluster_idx2 in range(cluster_idx1 + 1, min(cluster_idx1 + 4, size)):
+            dist_clust = pair_distance(center_area_clusters, cluster_idx1, cluster_idx2)
+            result = min(result, (dist_clust[0], center_area_clusters[cluster_idx1].horiz_center(), center_area_clusters[cluster_idx2].horiz_center()))
+    return result
+#                                                   horiz_pos, vert_pos, population, risk
+print closest_pair_strip([alg_cluster.Cluster(set([]), 0, 0, 1, 0),
+                          alg_cluster.Cluster(set([]), 0, 1, 1, 0),
+                          alg_cluster.Cluster(set([]), 0, 2, 1, 0),
+                          alg_cluster.Cluster(set([]), 0, 3, 1, 0)],
+                         0.0, 1.0)
 
-    return ()
-            
- 
-    
+#closest_pair_strip([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)], 1.5, 1.0) expected one of the tuples in set([(1.0, 1, 2)]) but received (Exception: TypeError) "unsupported operand type(s) for -: 'instancemethod' and 'float'" at line 92, in closest_pair_strip
+#closest_pair_strip([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 0, 1, 1, 0), alg_cluster.Cluster(set([]), 0, 2, 1, 0), alg_cluster.Cluster(set([]), 0, 3, 1, 0)], 0.0, 1.0) expected one of the tuples in set([(1.0, 2, 3), (1.0, 0, 1), (1.0, 1, 2)]) but received (1.0, 0, 0)
+# fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)]) expected one of the tuples in set([(1.0, 1, 2), (1.0, 0, 1), (1.0, 2, 3)]) but received (Exception: TypeError) "'tuple' object does not support item assignment" at line 71, in fast_closest_pair
+#print fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
+
+#print slow_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
+
+
+
 ######################################################################
 # Code for hierarchical clustering
 
