f4850fdab2fa9cf700707cacd297253287992d57
Gleb <31935368+lobzison@users.noreply.github.com>
2017-12-28 14:26:50 +0100

    Update hello_world.swift

--- warmups/hello_world.swift
+++ warmups/hello_world.swift
@@ -41,3 +41,42 @@ while i < 10 {
     print(i > 6 ? "more then 6" : "less than 6")
     i += 1
 }
+
+var i = 0
+print("conditional operator ? :")
+while i < 10 {
+    print(i)
+    print(i > 6 ? "more then 6" : "less than 6")
+    i += 1
+}
+
+print("nil coalesce")
+var n:String?
+print((n ?? "n is empty"))
+
+print("possible to coalsece to different datatype")
+var m:Double? = 1
+print(m ?? "m is empty")
+
+print("for range loop")
+var my_sum = 0
+for index_not_declared in 0...10 {
+    my_sum += index_not_declared
+    print(my_sum, index_not_declared)
+}
+
+print("half open range")
+my_sum = 0
+for index_not_declared in 0..<10 {
+    my_sum += index_not_declared
+    print(my_sum, index_not_declared)
+}
+
+print("example from documantation")
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count { // if youll use ... insted of ..< it will throw an error
+    print("Person \(i + 1) is called \(names[i])")
+}
+
+
