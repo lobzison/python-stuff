9507dced04c90233733e7a7b71a76dc1e85131c3
Глеб Лобов <lobzison@gmail.com>
2017-12-28 20:54:55 +0100

    added first clustering

--- AT/clustering.py
+++ AT/clustering.py
@@ -42,7 +42,7 @@ def slow_closest_pair(cluster_list):
     Output: tuple of the form (dist, idx1, idx2)
     where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2]
-    have minimum distance dist.       
+    have minimum distance dist.
     """
     result = (float("inf"), -1, -1)
     num_clusters = len(cluster_list)
@@ -91,41 +91,29 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
 
     Input: cluster_list is a list of clusters produced by fast_closest_pair
     horiz_center is the horizontal position of the strip's vertical center line
-    half_width is the half the width of the strip 
+    half_width is the half the width of the strip
     (i.e; the maximum horizontal distance
     that a cluster can lie from the center line)
 
     Output: tuple of the form (dist, idx1, idx2)
     where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] lie
-    in the strip and have minimum distance dist.       
+    in the strip and have minimum distance dist.
     """
     center_area_clusters = [cluster for cluster in cluster_list
-                            if abs(cluster.horiz_center() - horiz_center) < half_width]
+                            if abs(cluster.horiz_center() -
+                                   horiz_center) < half_width]
     center_area_clusters.sort(key=lambda cluster: cluster.vert_center())
     size = len(center_area_clusters)
     result = (float("inf"), -1, -1)
     for cluster_idx1 in range(size - 1):
-        for cluster_idx2 in range(cluster_idx1 + 1, min(cluster_idx1 + 4, size)):
+        for cluster_idx2 in range(cluster_idx1 + 1,
+                                  min(cluster_idx1 + 4, size)):
             orig_idx1 = cluster_list.index(center_area_clusters[cluster_idx1])
             orig_idx2 = cluster_list.index(center_area_clusters[cluster_idx2])
             dist_clust = pair_distance(cluster_list, orig_idx1, orig_idx2)
             result = min(result, dist_clust)
     return result
-#                                                   horiz_pos, vert_pos, population, risk
-
-
-print closest_pair_strip([alg_cluster.Cluster(set([]), 1.0, 1.0, 1, 0),
-                          alg_cluster.Cluster(set([]), 1.0, 5.0, 1, 0),
-                          alg_cluster.Cluster(set([]), 1.0, 4.0, 1, 0),
-                          alg_cluster.Cluster(set([]), 1.0, 7.0, 1, 0)],
-                         1.0, 3.0)
-
-# closest_pair_strip([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 0, 1, 1, 0), alg_cluster.Cluster(set([]), 0, 2, 1, 0), alg_cluster.Cluster(set([]), 0, 3, 1, 0)], 0.0, 1.0) expected one of the tuples in set([(1.0, 2, 3), (1.0, 0, 1), (1.0, 1, 2)]) but received (1.0, 0, 0)
-# fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)]) expected one of the tuples in set([(1.0, 1, 2), (1.0, 0, 1), (1.0, 2, 3)]) but received (Exception: TypeError) "'tuple' object does not support item assignment" at line 71, in fast_closest_pair
-# print fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
-
-# print slow_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
 
 
 ######################################################################
@@ -140,8 +128,12 @@ def hierarchical_clustering(cluster_list, num_clusters):
     Input: List of clusters, integer number of clusters
     Output: List of clusters whose length is num_clusters
     """
-
-    return []
+    clusters = [cluster.copy() for cluster in cluster_list]
+    while len(clusters) > num_clusters:
+        closest = fast_closest_pair(clusters)
+        clusters[closest[1]].merge_clusters(clusters[closest[2]])
+        clusters.pop(closest[2])
+    return clusters
 
 
 ######################################################################
@@ -153,10 +145,12 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     Compute the k-means clustering of a set of clusters
     Note: the function may not mutate cluster_list
 
-    Input: List of clusters, integers number of clusters and number of iterations
+    Input: List of clusters, integers number of clusters
+    and number of iterations
     Output: List of clusters whose length is num_clusters
     """
 
-    # position initial clusters at the location of clusters with largest populations
+    # position initial clusters at the location of clusters
+    # with largest populations
 
     return []
--- AT/unit_clustering.py
+++ AT/unit_clustering.py
@@ -1,5 +1,29 @@
 import clustering
+import alg_cluster
 import imp
 
 foo = imp.load_source('poc_simpletest', '../PoC/poc_simpletest.py')
 foo.TestSuite()
+
+
+#                                   horiz_pos, vert_pos, population, risk
+print clustering.closest_pair_strip([alg_cluster.Cluster(set([]), 1.0, 1.0, 1, 0),
+                                     alg_cluster.Cluster(
+                                         set([]), 1.0, 5.0, 1, 0),
+                                     alg_cluster.Cluster(
+                                         set([]), 1.0, 4.0, 1, 0),
+                                     alg_cluster.Cluster(set([]), 1.0, 7.0, 1, 0)],
+                                    1.0, 3.0)
+
+# closest_pair_strip([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 0, 1, 1, 0), alg_cluster.Cluster(set([]), 0, 2, 1, 0), alg_cluster.Cluster(set([]), 0, 3, 1, 0)], 0.0, 1.0) expected one of the tuples in set([(1.0, 2, 3), (1.0, 0, 1), (1.0, 1, 2)]) but received (1.0, 0, 0)
+# fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)]) expected one of the tuples in set([(1.0, 1, 2), (1.0, 0, 1), (1.0, 2, 3)]) but received (Exception: TypeError) "'tuple' object does not support item assignment" at line 71, in fast_closest_pair
+# print fast_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
+
+# print slow_closest_pair([alg_cluster.Cluster(set([]), 0, 0, 1, 0), alg_cluster.Cluster(set([]), 1, 0, 1, 0), alg_cluster.Cluster(set([]), 2, 0, 1, 0), alg_cluster.Cluster(set([]), 3, 0, 1, 0)])
+
+print clustering.hierarchical_clustering([alg_cluster.Cluster(set([]), 1.0, 1.0, 1, 0),
+                                          alg_cluster.Cluster(
+                                              set([]), 1.0, 5.0, 1, 0),
+                                          alg_cluster.Cluster(
+                                              set([]), 1.0, 4.0, 1, 0),
+                                          alg_cluster.Cluster(set([]), 1.0, 7.0, 1, 0)], 2)
