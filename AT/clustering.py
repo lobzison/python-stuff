14da0a4ad4c234acfb6d234fab4d93a198a6d8e8
Gleb <31935368+lobzison@users.noreply.github.com>
2017-12-29 10:41:23 +0100

    Update clustering.py

--- AT/clustering.py
+++ AT/clustering.py
@@ -1,13 +1,11 @@
 """
 Student template code for Project 3
 Student will implement five functions:
-
 slow_closest_pair(cluster_list)
 fast_closest_pair(cluster_list)
 closest_pair_strip(cluster_list, horiz_center, half_width)
 hierarchical_clustering(cluster_list, num_clusters)
 kmeans_clustering(cluster_list, num_clusters, num_iterations)
-
 where cluster_list is a 2D list of clusters in the plane
 """
 
@@ -22,10 +20,8 @@ def pair_distance(cluster_list, idx1, idx2):
     """
     Helper function that computes Euclidean distance
     between two clusters in a list
-
     Input: cluster_list is list of clusters,
     idx1 and idx2 are integer indices for two clusters
-
     Output: tuple (dist, idx1, idx2) where dist is distance between
     cluster_list[idx1] and cluster_list[idx2]
     """
@@ -36,9 +32,7 @@ def pair_distance(cluster_list, idx1, idx2):
 def slow_closest_pair(cluster_list):
     """
     Compute the distance between the closest pair of clusters in a list (slow)
-
     Input: cluster_list is the list of clusters
-
     Output: tuple of the form (dist, idx1, idx2)
     where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2]
@@ -58,11 +52,9 @@ def slow_closest_pair(cluster_list):
 def fast_closest_pair(cluster_list):
     """
     Compute the distance between the closest pair of clusters in a list (fast)
-
     Input: cluster_list is list of clusters
     SORTED such that horizontal positions of their
     centers are in ascending order
-
     Output: tuple of the form (dist, idx1, idx2)
     where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2]
@@ -88,13 +80,11 @@ def fast_closest_pair(cluster_list):
 def closest_pair_strip(cluster_list, horiz_center, half_width):
     """
     Helper function to compute the closest pair of clusters in a vertical strip
-
     Input: cluster_list is a list of clusters produced by fast_closest_pair
     horiz_center is the horizontal position of the strip's vertical center line
     half_width is the half the width of the strip
     (i.e; the maximum horizontal distance
     that a cluster can lie from the center line)
-
     Output: tuple of the form (dist, idx1, idx2)
     where the centers of the clusters
     cluster_list[idx1] and cluster_list[idx2] lie
@@ -124,12 +114,12 @@ def hierarchical_clustering(cluster_list, num_clusters):
     """
     Compute a hierarchical clustering of a set of clusters
     Note: the function may mutate cluster_list
-
     Input: List of clusters, integer number of clusters
     Output: List of clusters whose length is num_clusters
     """
     clusters = [cluster.copy() for cluster in cluster_list]
     while len(clusters) > num_clusters:
+        clusters.sort(key=lambda cluster: cluster.horiz_center())
         closest = fast_closest_pair(clusters)
         clusters[closest[1]].merge_clusters(clusters[closest[2]])
         clusters.pop(closest[2])
@@ -144,7 +134,6 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     """
     Compute the k-means clustering of a set of clusters
     Note: the function may not mutate cluster_list
-
     Input: List of clusters, integers number of clusters
     and number of iterations
     Output: List of clusters whose length is num_clusters
