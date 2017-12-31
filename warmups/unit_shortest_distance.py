3d56c8d3971701946c4d8b01668a4c20be3a3735
Глеб Лобов <lobzison@gmail.com>
2017-12-31 11:27:31 +0100

    added tests

--- warmups/unit_shortest_distance.py
+++ warmups/unit_shortest_distance.py
@@ -1,5 +1,6 @@
 import shortest_distance as sd
 import unittest
+import math
 
 
 class DistanceTest(unittest.TestCase):
@@ -36,6 +37,14 @@ class DistanceTest(unittest.TestCase):
     def test_y4(self):
         """Distance between 0,0 and 0,-3 == 3"""
         self.assertEqual(sd.e_distance((0, 0), (-3, 0)), 3)
+
+    def test_x_y1(self):
+        """Distance between 1,1 and 0,0 == sqrt(2)"""
+        self.assertEqual(sd.e_distance((1, 1), (0, 0)), math.sqrt(2))
+
+    def test_x_y2(self):
+        """Distance between 18,-3 and -2,6 == sqrt(481)"""
+        self.assertEqual(sd.e_distance((18, -3), (-2, 6)), math.sqrt(481))
 
 
 if __name__ == '__main__':
