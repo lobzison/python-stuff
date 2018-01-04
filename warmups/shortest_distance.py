aeda11d1b8f4c131fa230dfab7e3f9154af99e48
Глеб Лобов <lobzison@gmail.com>
2018-01-04 08:32:42 +0100

    comment

--- warmups/shortest_distance.py
+++ warmups/shortest_distance.py
@@ -13,14 +13,13 @@ def e_distance(point1, point2):
 
 def brute_force(points_list):
     """Takes list of tuples of format (x, y),
-       returns a tuple of format (distance, (x1, y1), (x2, y2))"""
+       returns a tuple of format (distance, (x1, y1), (x2, y2))
+       takes O(N)"""
     min_dist = (float("inf"), (None, None), (None, None))
     for point1 in points_list:
         for point2 in points_list:
             new_dist = (e_distance(point1, point2), point1, point2)
-            print new_dist
             if new_dist[0] < min_dist[0] and point1 is not point2:
                 min_dist = new_dist
     return min_dist
 
-print brute_force([(64, -59), (-1, -15), (34, 32), (51, 55), (65, 14), (-98, 80), (-21, -23)])
