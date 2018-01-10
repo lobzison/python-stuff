de16a0c61602fa7dcdc7db585d126dce5dd807b6
Глеб Лобов <lobzison@gmail.com>
2018-01-10 18:34:48 +0100

    fixed the alg

--- AT/clustering.py
+++ AT/clustering.py
@@ -63,8 +63,8 @@ def fast_closest_pair(cluster_list):
     if num_clusters <= 3:
         return slow_closest_pair(cluster_list)
     middle = num_clusters // 2
-    left_part = [cluster for cluster in cluster_list[:middle]]
-    right_part = [cluster for cluster in cluster_list[middle:]]
+    left_part = cluster_list[:middle]
+    right_part = cluster_list[middle:]
     result_left = fast_closest_pair(left_part)
     result_right = fast_closest_pair(right_part)
     result = min(
@@ -89,18 +89,18 @@ def closest_pair_strip(cluster_list, horiz_center, half_width):
     cluster_list[idx1] and cluster_list[idx2] lie
     in the strip and have minimum distance dist.
     """
-    center_area_clusters = [cluster for cluster in cluster_list
-                            if abs(cluster.horiz_center() -
-                                   horiz_center) < half_width]
-    center_area_clusters.sort(key=lambda cluster: cluster.vert_center())
-    size = len(center_area_clusters)
+    center_area_indexes = [index for index in xrange(len(cluster_list))
+                           if abs(cluster_list[index].horiz_center() -
+                           horiz_center) < half_width]
+    center_area_indexes.sort(key=lambda x: cluster_list[x].vert_center())
+    size = len(center_area_indexes)
     result = (float("inf"), -1, -1)
-    for cluster_idx1 in range(size - 1):
-        for cluster_idx2 in range(cluster_idx1 + 1,
-                                  min(cluster_idx1 + 4, size)):
-            orig_idx1 = cluster_list.index(center_area_clusters[cluster_idx1])
-            orig_idx2 = cluster_list.index(center_area_clusters[cluster_idx2])
-            dist_clust = pair_distance(cluster_list, orig_idx1, orig_idx2)
+    for cluster_idx1 in xrange(size - 1):
+        for cluster_idx2 in xrange(cluster_idx1 + 1,
+                                   min(cluster_idx1 + 4, size)):
+            dist_clust = pair_distance(cluster_list,
+                                       cluster_idx1,
+                                       cluster_idx2)
             result = min(result, dist_clust)
     return result
 
@@ -122,6 +122,7 @@ def hierarchical_clustering(cluster_list, num_clusters):
         closest = fast_closest_pair(clusters)
         clusters[closest[1]].merge_clusters(clusters[closest[2]])
         clusters.pop(closest[2])
+        print len(clusters)
     return clusters
 
 
