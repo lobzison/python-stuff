223f18c8fec13c32318a5a4d2c2f183584fa4aa4
Глеб Лобов <lobzison@gmail.com>
2018-01-15 21:31:37 +0100

    added start for the sequences

---
+++ dna_seqence.py
@@ -0,0 +1,29 @@
+"""
+Implementation of recursive and DP algorithms
+for RNA secondary structure problem
+"""
+
+
+def is_legit(first_letter, second_letter):
+    """
+    Check if two structures can be folded
+    """
+    l_one = min(first_letter, second_letter)
+    l_two = max(first_letter, second_letter)
+    if (l_one == "A" and l_two == "U" or
+            l_one == "C" and l_two == "G"):
+        return True
+    else:
+        return False
+
+
+def recursive_implementation(rna_sequence):
+    """
+    Recursive implementation of rna sequence problem
+    returns the max number of folds
+    """
+    if len(rna_sequence) <= 4:
+        return 0
+    #res = max(recursive_implementation(rna_sequence[:-1]))
+
+print "asdfgghhj"[:-1]
