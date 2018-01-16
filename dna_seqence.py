ed9e035da6f23fdd5589c7642ea85cce57ba476b
Глеб Лобов <lobzison@gmail.com>
2018-01-16 22:33:42 +0100

    moved staff, added dictionary generator implementation

--- dna_seqence.py
+++ dna_seqence.py
@@ -27,25 +27,6 @@ def recursive_implementation(rna_sequence):
     # res = max(recursive_implementation(rna_sequence[:-1]), )
 
 
-def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
-    """
-    Builds a scoring matrix in a form of dictionay of dictionaries
-    returns the matrix
-    """
-    extended_alphabet = "-" + alphabet
-    outside_dict = {}
-    for outside_letter in extended_alphabet:
-        inside_dict = {}
-        for inside_letter in extended_alphabet:
-            if outside_letter == "-" or inside_letter == "-":
-                inside_dict[inside_letter] = dash_score
-            elif outside_letter == inside_letter:
-                inside_dict[inside_letter] = diag_score
-            else:
-                inside_dict[inside_letter] = off_diag_score
-        outside_dict[outside_letter] = inside_dict
-    return outside_dict
 
-a = build_scoring_matrix("abcd", 10, 1, -1)
 
 print a
