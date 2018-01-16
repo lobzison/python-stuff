525ea11ac9c869a36f86b94edd18346a9d9d7ad4
Глеб Лобов <lobzison@gmail.com>
2018-01-16 22:25:25 +0100

    added scoring function

--- dna_seqence.py
+++ dna_seqence.py
@@ -24,6 +24,28 @@ def recursive_implementation(rna_sequence):
     """
     if len(rna_sequence) <= 4:
         return 0
-    #res = max(recursive_implementation(rna_sequence[:-1]))
+    # res = max(recursive_implementation(rna_sequence[:-1]), )
 
-print "asdfgghhj"[:-1]
+
+def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
+    """
+    Builds a scoring matrix in a form of dictionay of dictionaries
+    returns the matrix
+    """
+    extended_alphabet = "-" + alphabet
+    outside_dict = {}
+    for outside_letter in extended_alphabet:
+        inside_dict = {}
+        for inside_letter in extended_alphabet:
+            if outside_letter == "-" or inside_letter == "-":
+                inside_dict[inside_letter] = dash_score
+            elif outside_letter == inside_letter:
+                inside_dict[inside_letter] = diag_score
+            else:
+                inside_dict[inside_letter] = off_diag_score
+        outside_dict[outside_letter] = inside_dict
+    return outside_dict
+
+a = build_scoring_matrix("abcd", 10, 1, -1)
+
+print a
